1ea85ccac022f9ad3286bf869964d9b33c1814d1
thomgasa <thomasjgamboas@protonmail.com>
2021-01-03 23:10:34 +0000

    Finished exercise

---
+++ src/js/component/navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const NavBar = () => {
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+				<a className="navbar-brand padding" href="#">
+					Star Bootstrap
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNavDropdown"
+					aria-controls="navbarNavDropdown"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarNavDropdown">
+					<ul className="navbar-nav ml-auto mr-5">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item ml-auto pr">
+							<a className="nav-link" href="#">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Services
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+};
+export default NavBar;
